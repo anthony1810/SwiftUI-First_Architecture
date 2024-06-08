6ad1e8e76065e0dba1a4ef2e28e84a6ef8a4562d
Quang Tran <quang.tran@appetiser.com.au>
2024-06-08 10:30:59 +0700

    Initial Commit

---
+++ Vocap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vocap
+//
+//  Created by Anthony Tran on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vocap/VocapApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocapApp.swift
+//  Vocap
+//
+//  Created by Anthony Tran on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct VocapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
